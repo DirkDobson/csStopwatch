a513ed33f90e459db1f64be9825eb8f26a356ccc
Dirk Dobson <dirkvancouver@live.com>
2019-02-08 19:13:01 -0700

    done

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace Timer
         static void Main(string[] args)
         {
             Stopwatch stopWatch = new Stopwatch();
-            System.Console.WriteLine("press anybutton to start or stop or type quit to exit");
+            System.Console.WriteLine("Press anybutton to start or stop or type quit to exit");
             string input = Console.ReadLine();
             bool start = false;
             while ( input != "quit" )
